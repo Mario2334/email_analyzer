5e3625984ff164e151bc6cf7bdfa3f2889b3956a
Mario2334 <sanketm221995@yahoo.co.in>
2017-06-21 22:07:54 +0530

    update word frequency test
    Signed-off-by: Mario2334 <sanketm221995@yahoo.co.in>

--- emails.py
+++ emails.py
@@ -1,12 +1,12 @@
 import base64
 import email
-import os
 
 try:
     import dominate
     import dominate.tags as tags
     import langdetect
     from bs4 import BeautifulSoup
+
 except ImportError:
     base_call = ['pip', 'install']
     libs = ['bs4', 'langdetect']
@@ -15,6 +15,7 @@ except ImportError:
     for i in libs:
         subprocess.call([base_call[0], base_call[1], i])
     pass
+
 import re
 
 ''' In this module I parsed all the emails in given set,decode it to readable to langdetect
@@ -25,16 +26,19 @@ and save results in an html file
 
 
 class parser(object):
-    def __init__(self, location, filename):
+    def __init__(self, filename):  # input resuls location an
         self.name = filename
-        self.file = open(location + '\\' + self.name)
+        self.file = open(os.getcwd() + '\\mails\\' + self.name)
         self.parser = 'lxml'
 
     def run(self):
+        langdetect.DetectorFactory.seed = 0
         self.dictionary = dict()
-        decoded = self.decode_data()
-        soup = BeautifulSoup(decoded, self.parser)
-        all_text = [self.clean_string(i.text) for i in soup.find_all('p')]
+        email_content = email.message_from_string(self.file.read())
+        message_data = email_content.get_payload()[1]._payload
+        decoded = self.decode_data(message_data)
+
+        all_text = self.extract_from_html(decoded)
         for z in all_text:
             cleaned = re.split(r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s', z)
             for string in cleaned:
@@ -52,6 +56,11 @@ class parser(object):
 
         self.to_html(self.dictionary)
 
+    def extract_from_html(self, decoded):
+        soup = BeautifulSoup(decoded)
+        all_text = [self.clean_string(i.text) for i in soup.find_all('p')]
+        return all_text
+
     def clean_string(self, text):
         return re.sub(pattern='\s*\n\s*', repl='', string=text)
 
@@ -67,18 +76,21 @@ class parser(object):
             data += b'=' * (4 - missing_padding)
         return base64.decodebytes(data)
 
-    def decode_data(self):
-        email_content = email.message_from_string(self.file.read())
-        message_data = email_content.get_payload()[0]._payload
-        print(message_data)
+    def decode_data(self, message_data):
+
         if '<' in message_data:
-            return message_data
+            a = message_data
         else:
             message_data = str.encode(message_data)
+
             try:
-                return self.decode_base64(message_data).decode('ascii')
+                a = self.decode_base64(message_data).decode('ascii')
             except:
-                return message_data
+                a = message_data
+        file = open(os.getcwd() + '\\html\\' + self.name + '.html', 'w+')
+        file.write(str(a))
+        file.close()
+        return a
 
     def to_html(self, data):
 
@@ -97,19 +109,18 @@ class parser(object):
                     a.add(tags.td(tags.p(i)))
                     a.add(tags.td(tags.p(data[i])))
         file_location = os.getcwd()
-        file = open(file_location + '\\data\\results\\' + self.name + '.html', 'w+')
+        file = open(file_location + '\\results\\' + self.name + '.html', 'w+')
         file.write(doc.render())
         file.close()
 
 
-'''
 if __name__ == '__main__':
     import os
     curr_mails = os.getcwd() + '\\data\mails'
+    os.chdir(os.getcwd() + '\\data')
     dataset = os.listdir(curr_mails)
     base_call = ['pip', 'install']
     libs = ['bs4', 'langdetect']
     for i in dataset:
-        parser(curr_mails ,i).run()
-
-'''
+        parser(i).run()
+        pass
